3f3e471dac1688d7c42f75650a7724a2fff2ebdf
Het <hra.opash@gmail.com>
2026-05-12 12:18:20 +0530

    Frontend : Redux Setup, Routing, apiRequest instance

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import apiRequest from "./utils/apiRequest";
+import { useDispatch } from "react-redux";
+import { setCheckingAuth, setCurrentUser } from "./features/usersSlice";
+import { Route, Routes } from "react-router";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Login from "./pages/Login";
 
 const App = () => {
-  return (
-    <div className='text-red-700'>
-      App
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await apiRequest.get("/users");
+        const user = response.data.data;
+        dispatch(setCurrentUser(user));
+      } catch (err) {
+        dispatch(setCheckingAuth(false));
+      }
+    };
+    checkAuth();
+  }, [dispatch]);
 
-export default App
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<ProtectedRoute />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
